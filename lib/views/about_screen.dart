a77d65bb9e9d8dd20358ddb5e64134cdf185c082
Aman Bafna <aman@brandzgarage.com>
2021-01-20 09:52:17 +0530

    about us url changed

--- lib/views/about_screen.dart
+++ lib/views/about_screen.dart
@@ -15,10 +15,10 @@ class _AboutScreenState extends State<AboutScreen> {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        "/": (_) =>  new WebviewScaffold(
-          url: "${ApiProvider.BASE_URL}about",
-          appBar:appBar(context, "About")
-        ),
+        "/": (_) => new WebviewScaffold(
+              url: "${ApiProvider.BASE_URL_FOR_WEB}about",
+              appBar: appBar(context, "About"),
+            ),
       },
       debugShowCheckedModeBanner: false,
     );
@@ -29,20 +29,31 @@ class _AboutScreenState extends State<AboutScreen> {
       preferredSize: Size.fromHeight(80),
       child: Container(
         color: Colors.white,
-       // height: 80,
+        // height: 80,
         width: getWidth(context),
         alignment: Alignment.center,
         child: Padding(
-          padding: const EdgeInsets.only(right:30.0,left: 30.0,top: 30),
+          padding: const EdgeInsets.only(right: 30.0, left: 30.0, top: 30),
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               InkWell(
-                  onTap: ()=> Navigator.pop(context),
-
-                  child: Icon(Icons.keyboard_backspace,color: icon_color,)),
-              SizedBox(width: 10,),
-              Text(s,style: TextStyle(color: text_color,fontSize: 15,fontFamily: "Manrope",fontWeight: FontWeight.bold),),
+                  onTap: () => Navigator.pop(context),
+                  child: Icon(
+                    Icons.keyboard_backspace,
+                    color: icon_color,
+                  )),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                s,
+                style: TextStyle(
+                    color: text_color,
+                    fontSize: 15,
+                    fontFamily: "Manrope",
+                    fontWeight: FontWeight.bold),
+              ),
             ],
           ),
         ),
